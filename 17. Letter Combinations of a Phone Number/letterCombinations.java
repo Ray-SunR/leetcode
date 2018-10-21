abde22c2c87af57b1bbae7ba11127942fe71d34c
Renchen Sun <renchen@amazon.com>
2018-10-20 23:37:43 -0700

    Solved 17

---
+++ 17. Letter Combinations of a Phone Number/letterCombinations.java
@@ -0,0 +1,31 @@
+class Solution {
+    private List<String> generate(final Character c, final List<String> combinations) {
+        final List<String> KEYBOARD_2 = new ArrayList<String>() {{
+        add(""); add("abc"); add("def"); add("ghi"); add("jkl"); add("mno"); add("pqrs"); add("tuv"); add("wxyz");
+        }};
+        final String chars = KEYBOARD_2.get(c - '1');
+        if (combinations.isEmpty()) {
+            final List<String> result = new ArrayList<>();
+            for (char ch : chars.toCharArray()) {
+                result.add(Character.toString(ch));
+            }
+            return result;
+        }
+        
+        final List<String> result = new ArrayList<>();
+        for (char ch : chars.toCharArray()) {
+            for (final String s : combinations) {
+                result.add(s + Character.toString(ch));
+            }
+        }
+        return result;
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        for (int i = 0; i < digits.length(); i++) {
+            combinations = generate(digits.charAt(i), combinations);
+        }
+        return combinations;
+    }
+}
