75111c73781a29b5b401dd89206a1f942872d426
Renchen Sun <sunrenchen@gmail.com>
2018-10-21 16:30:16 -0700

    Solved 20

---
+++ 20. Valid Parentheses/isValidParentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        final Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty()) { return false; }
+                switch (s.charAt(i)) {
+                    case ')':
+                        if (stack.pop() != '(') { return false; }
+                        break;
+                    case '}':
+                        if (stack.pop() != '{') { return false; }
+                        break;
+                    case ']':
+                        if (stack.pop() != '[') { return false; }
+                        break;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
