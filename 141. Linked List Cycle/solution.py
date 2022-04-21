adce2bf0ea22d2a929a090359031556d2861679f
Renchen Sun <sunrenchen@gmail.com>
2022-04-21 10:53:29 -0700

    solved 141

---
+++ 141. Linked List Cycle/solution.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        one = head
+        two = head.next
+        while one and two:
+            if one == two:
+                return True
+            one = one.next
+            two = two.next
+            if two:
+                two = two.next
+        return False
