64d1bf1e725b4d77d378bfb4a4e312ec7c9de996
Renchen Sun <null@Yuchens-MacBook-Air.local>
2024-01-06 21:07:57 -0800

    solved

---
+++ 3620 (LintCode) · Logger Rate Limiter/solution.py
@@ -0,0 +1,15 @@
+class Logger:
+    def __init__(self):
+        self.d = {}
+    """
+    @param timestamp: Timestamp
+    @param message: Message
+    @return: Whether the log can be printed
+    """
+    def could_print_message(self, timestamp: int, message: str) -> bool:
+        if message not in self.d or timestamp - self.d[message] >= 10:
+            self.d[message] = timestamp
+            return True
+        
+        return False
+        
